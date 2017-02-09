3f45888445a87d93c1b09d3fa6d46a69ad698c2a
Jonathas Lima <alexandrejonathas@gmail.com>
2017-02-09 12:36:06 -0300

    Comitando a validação do apelido

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,15 @@
-module.exports.chat = function(application, req, res){
+module.exports.iniciaChat = function(application, req, res){
+    var usuario = req.body;
+
+    req.assert('apelido', 'O apelido é obrigatório!').notEmpty();
+    req.assert('apelido', 'O apelido deve conter entre 3 e 10 caracteres').len(3, 10);
+
+    var errors = req.validationErrors();
+
+    if(errors){
+      res.render('home/index', {errors: errors, usuario: usuario});
+      return;
+    }
+
     res.render('chat/chat');
 }
